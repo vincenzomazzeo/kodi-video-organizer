501d7354db8350548982c9c4f8df4b9b398c7810
vincenzomazzeo <vincenzo.mazzeo@gmail.com>
2015-06-30 12:13:00 +0200

    .

---
+++ src/main/java/it/ninjatech/kvo/ui/exception/ExceptionView.java
@@ -0,0 +1,9 @@
+package it.ninjatech.kvo.ui.exception;
+
+import com.alee.laf.rootpane.WebDialog;
+
+public class ExceptionView extends WebDialog {
+
+	private static final long serialVersionUID = 4772626517954754101L;
+
+}
